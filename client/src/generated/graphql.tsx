a39ec581b696b450481814631410d732e2cdb100
PRSMendis <rayhan.mendis@gmail.com>
2023-08-23 15:00:42 +1000

    hooked up frontend to persist data

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -15,6 +15,17 @@ export type Scalars = {
   Float: number;
 };
 
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTweet: Tweet;
+};
+
+
+export type MutationCreateTweetArgs = {
+  body: Scalars['String'];
+  userId: Scalars['String'];
+};
+
 export type Query = {
   __typename?: 'Query';
   currentUser: User;
@@ -71,6 +82,19 @@ export type GetCurrentUserQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type GetCurrentUserQuery = { __typename?: 'Query', currentUser: { __typename?: 'User', id: string, name: string, handle: string, avatarUrl: string, createdAt: string, stats?: { __typename?: 'UserStats', tweetCount: number, followingCount: number, followerCount: number } | null }, suggestions: Array<{ __typename?: 'Suggestion', name: string, handle: string, avatarUrl: string, reason: string }> };
 
+export type CreateNewTweetMutationVariables = Exact<{
+  userId: Scalars['String'];
+  body: Scalars['String'];
+}>;
+
+
+export type CreateNewTweetMutation = { __typename?: 'Mutation', createTweet: { __typename?: 'Tweet', id: string } };
+
+export type GetTimelineTweetsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetTimelineTweetsQuery = { __typename?: 'Query', tweets: Array<{ __typename?: 'Tweet', id: string, body: string, createdAt: string, stats?: { __typename?: 'TweetStats', favoriteCount: number, retweetCount: number, commentCount: number } | null, author?: { __typename?: 'User', name: string, handle: string, avatarUrl: string } | null }> };
+
 
 export const GetCurrentUserDocument = gql`
     query GetCurrentUser {
@@ -121,3 +145,83 @@ export function useGetCurrentUserLazyQuery(baseOptions?: Apollo.LazyQueryHookOpt
 export type GetCurrentUserQueryHookResult = ReturnType<typeof useGetCurrentUserQuery>;
 export type GetCurrentUserLazyQueryHookResult = ReturnType<typeof useGetCurrentUserLazyQuery>;
 export type GetCurrentUserQueryResult = Apollo.QueryResult<GetCurrentUserQuery, GetCurrentUserQueryVariables>;
+export const CreateNewTweetDocument = gql`
+    mutation CreateNewTweet($userId: String!, $body: String!) {
+  createTweet(userId: $userId, body: $body) {
+    id
+  }
+}
+    `;
+export type CreateNewTweetMutationFn = Apollo.MutationFunction<CreateNewTweetMutation, CreateNewTweetMutationVariables>;
+
+/**
+ * __useCreateNewTweetMutation__
+ *
+ * To run a mutation, you first call `useCreateNewTweetMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateNewTweetMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createNewTweetMutation, { data, loading, error }] = useCreateNewTweetMutation({
+ *   variables: {
+ *      userId: // value for 'userId'
+ *      body: // value for 'body'
+ *   },
+ * });
+ */
+export function useCreateNewTweetMutation(baseOptions?: Apollo.MutationHookOptions<CreateNewTweetMutation, CreateNewTweetMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateNewTweetMutation, CreateNewTweetMutationVariables>(CreateNewTweetDocument, options);
+      }
+export type CreateNewTweetMutationHookResult = ReturnType<typeof useCreateNewTweetMutation>;
+export type CreateNewTweetMutationResult = Apollo.MutationResult<CreateNewTweetMutation>;
+export type CreateNewTweetMutationOptions = Apollo.BaseMutationOptions<CreateNewTweetMutation, CreateNewTweetMutationVariables>;
+export const GetTimelineTweetsDocument = gql`
+    query GetTimelineTweets {
+  tweets {
+    id
+    body
+    stats {
+      favoriteCount
+      retweetCount
+      commentCount
+    }
+    createdAt
+    author {
+      name
+      handle
+      avatarUrl
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetTimelineTweetsQuery__
+ *
+ * To run a query within a React component, call `useGetTimelineTweetsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetTimelineTweetsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetTimelineTweetsQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetTimelineTweetsQuery(baseOptions?: Apollo.QueryHookOptions<GetTimelineTweetsQuery, GetTimelineTweetsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetTimelineTweetsQuery, GetTimelineTweetsQueryVariables>(GetTimelineTweetsDocument, options);
+      }
+export function useGetTimelineTweetsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetTimelineTweetsQuery, GetTimelineTweetsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetTimelineTweetsQuery, GetTimelineTweetsQueryVariables>(GetTimelineTweetsDocument, options);
+        }
+export type GetTimelineTweetsQueryHookResult = ReturnType<typeof useGetTimelineTweetsQuery>;
+export type GetTimelineTweetsLazyQueryHookResult = ReturnType<typeof useGetTimelineTweetsLazyQuery>;
+export type GetTimelineTweetsQueryResult = Apollo.QueryResult<GetTimelineTweetsQuery, GetTimelineTweetsQueryVariables>;
