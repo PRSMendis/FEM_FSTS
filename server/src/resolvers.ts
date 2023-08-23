49c8520d25a488c413771672887b5f02b768dc97
PRSMendis <rayhan.mendis@gmail.com>
2023-08-23 11:11:23 +1000

    working on resolvers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,6 +1,8 @@
 import { Resolvers } from 'resolvers-types.generated';
 import Query from './resolvers/Query';
 import Db, { DbTweet, DbUser } from './db';
+import Tweet from './resolvers/Tweet';
+import User from './resolvers/User';
 
 export interface TwitterResolverContext {
     db: Db,
@@ -10,6 +12,8 @@ export interface TwitterResolverContext {
   }
 const resolvers: Resolvers<TwitterResolverContext> =  {
     Query,
+    Tweet,
+    User,
   }
 
 export default resolvers;
